d3a2b8561872ca0c3480eede2fde7e0820c05e6e
wasupon <wasupon.mail@gmail.com>
2024-01-02 17:09:03 +0900

    Add: search combo-box

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,10 @@ import Hero from './Hero'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import CustomButton from './CustomButton'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+import SearchManufacturer from './SearchManufacturer'
+
 export {
-    Hero, CustomButton, Footer, Navbar
+    Hero, CustomButton, Footer, Navbar, SearchBar, CustomFilter, SearchManufacturer
 }
