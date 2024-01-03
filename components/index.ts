b9f7426975e217b61e153fa2cf981606d3f3ddaf
wasupon <wasupon.mail@gmail.com>
2024-01-03 15:05:35 +0900

    Add: model card for each item

--- components/index.ts
+++ components/index.ts
@@ -5,7 +5,9 @@ import CustomButton from './CustomButton'
 import SearchBar from './SearchBar'
 import CustomFilter from './CustomFilter'
 import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
 
 export {
-    Hero, CustomButton, Footer, Navbar, SearchBar, CustomFilter, SearchManufacturer
+    Hero, CustomButton, Footer, Navbar, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails
 }
