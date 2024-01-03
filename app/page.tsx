fbca3f9ec33b60dfb4c20d979e8f1ee9b26294f2
wasupon <wasupon.mail@gmail.com>
2024-01-03 19:11:25 +0900

    Modify: setFilter interface to be type any

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,6 @@ export default function Home() {
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-            <p>{allCars?.message}</p>
           </div>
         )}
       </div>
--- types/index.ts
+++ types/index.ts
@@ -13,18 +13,6 @@ export interface CarProps {
   model: string;
   transmission: string;
   year: number;
-  city_mpg: number;
-  class: string;
-  combination_mpg: number;
-  cylinders: number;
-  displacement: number;
-  drive: string;
-  fuel_type: string;
-  highway_mpg: number;
-  make: string;
-  model: string;
-  transmission: string;
-  year: number;
 }
 
 export interface FilterProps {
@@ -67,7 +55,7 @@ export interface OptionProps {
 export interface CustomFilterProps {
   title: string;
   options: OptionProps[];
-  setFilter: (value: number | string) => void;
+  setFilter: (value: any) => void;
 }
 
 export interface ShowMoreProps {
