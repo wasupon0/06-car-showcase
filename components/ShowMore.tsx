8b282ec1c564184d9e607d35bb387316c6183226
wasupon <wasupon.mail@gmail.com>
2024-01-03 19:22:49 +0900

    Delete: import of updateSearchParams

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,7 +3,6 @@
 import { useRouter } from "next/navigation";
 
 import { ShowMoreProps } from "@/types";
-import { updateSearchParams } from "@/utils";
 import { CustomButton } from "@/components";
 
 const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
